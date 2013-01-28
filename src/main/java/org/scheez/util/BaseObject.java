7c3fa11732d710d8487c30fc87b6233c69cb4cec
Eric Scheie <eric@continuuity.com>
2013-01-27 17:09:35 -0800

    Loading data into compiled class as part of the unit test.

--- src/main/java/org/scheez/util/BaseObject.java
+++ src/main/java/org/scheez/util/BaseObject.java
@@ -1,5 +1,9 @@
 package org.scheez.util;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -7,7 +11,6 @@ import org.apache.commons.lang.builder.ToStringStyle;
 
 public class BaseObject
 {
-
     @Override
     public int hashCode()
     {
@@ -17,7 +20,7 @@ public class BaseObject
     @Override
     public boolean equals(Object obj)
     {
-        boolean equals = false;    
+        boolean equals = false;
         if (obj != null)
         {
             equals = EqualsBuilder.reflectionEquals(this, obj);
@@ -28,7 +31,51 @@ public class BaseObject
     @Override
     public String toString()
     {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+    public boolean hasNulls()
+    {
+        boolean nulls = false;
+        try
+        {
+            BeanInfo beanInfo = Introspector.getBeanInfo(getClass());
+            for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors())
+            {
+                if (descriptor.getReadMethod().invoke(this) == null)
+                {
+                    nulls = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        return nulls;
     }
     
+    public boolean allNulls()
+    {
+        boolean nulls = true;
+        try
+        {
+            BeanInfo beanInfo = Introspector.getBeanInfo(getClass());
+            for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors())
+            {
+                if (descriptor.getReadMethod().invoke(this) != null)
+                {
+                    nulls = false;
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        return nulls;
+    }
 }
--- src/test/java/org/scheez/classgen/ClassGeneratorTest.java
+++ src/test/java/org/scheez/classgen/ClassGeneratorTest.java
@@ -3,20 +3,25 @@ package org.scheez.classgen;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collection;
+import java.util.List;
 
 import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
-import javax.tools.JavaCompiler.CompilationTask;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.scheez.map.RowMapper;
 import org.scheez.schema.objects.TableName;
 import org.scheez.test.db.TestDatabase;
 import org.scheez.test.db.TestDatabaseManager;
+import org.scheez.util.BaseObject;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @RunWith(Parameterized.class)
@@ -29,12 +34,14 @@ public class ClassGeneratorTest
         this.testDatabase = testDatabase;
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Test
-    public void testClassGenerator()
+    public void testClassGenerator() throws Exception
     {
         File srcDir = new File("build/tmp/ClassGenerator");
         srcDir.mkdirs();
         
+        URLClassLoader cl = new URLClassLoader(new URL[] { srcDir.toURI().toURL() });
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
         
@@ -42,12 +49,24 @@ public class ClassGeneratorTest
         JdbcTemplate template = new JdbcTemplate(testDatabase.getDataSource());
         for (TableName tableName : testDatabase.getSystemTableNames())
         {
+            String sql = "SELECT * FROM " + tableName;
             String clsName = "org.scheez.test." + testDatabase.getName() + "." + tableName;
-            File clsFile = template.query("SELECT * FROM " + tableName, codeGenerator.generateClass(clsName));
+            File clsFile = template.query(sql, codeGenerator.generateClass(clsName));
             assertNotNull(clsFile);
             
             CompilationTask task = compiler.getTask(null, fileManager, null, null, null, fileManager.getJavaFileObjects(clsFile));
             assertTrue(task.call());
+           
+            Class<?> cls = cl.loadClass(clsName);
+            assertNotNull(cls);
+
+            List<? extends BaseObject> list = template.query(sql, new RowMapper(cls));
+            assertNotNull(list);
+            
+            for (BaseObject obj : list)
+            {
+                assertFalse(obj.allNulls());
+            }
         }
     }
     
